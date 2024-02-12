f4fd98ae7bc00c4a54a2832a248b8ed2dc729e13
Yuri Barsotti <yuribarsotti@gmail.com>
2024-02-11 23:25:53 -0300

    add falling platform

--- scripts/player.gd
+++ scripts/player.gd
@@ -41,6 +41,11 @@ func _physics_process(delta):
 	
 	_set_state()
 	move_and_slide()
+	
+	for platforms in get_slide_collision_count():
+		var collision = get_slide_collision(platforms)
+		if collision.get_collider().has_method("has_collided_with"):
+			collision.get_collider().has_collided_with(collision, self)
 
 
 func _on_hurtbox_body_entered(body: Node2D):	
