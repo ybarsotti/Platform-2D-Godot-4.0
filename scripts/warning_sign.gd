9300c7e38395918a14873e99b18341eef9214a8d
Yuri Barsotti <yuribarsotti@gmail.com>
2024-02-25 22:50:23 -0300

    add animated dialog

--- scripts/warning_sign.gd
+++ scripts/warning_sign.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var texture = $texture
 @onready var area_sign = $area_sign
+@onready var animation_player = $AnimationPlayer as AnimationPlayer
 
 const lines : Array[String] = [
 	"Olá, aventureiro!",
@@ -15,10 +16,12 @@ func _unhandled_input(event):
 	if area_sign.get_overlapping_bodies().size() > 0:
 		texture.show()
 		if event.is_action_pressed("interaction") && !DialogManager.is_message_active:
+			animation_player.stop()
 			texture.hide()
 			DialogManager.start_message(global_position, lines)
 	else:
 		texture.hide()
+		animation_player.play("float")
 		if DialogManager.dialog_box != null:
 			DialogManager.dialog_box.queue_free()
 			DialogManager.is_message_active = false
