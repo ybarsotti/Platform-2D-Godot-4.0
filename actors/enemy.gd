64d2c07c6968f8e01cc390c29f5ef67b6b23e3cc
Yuri Barsotti <yuribarsotti@gmail.com>
2024-03-30 21:23:06 -0300

    add enemy base class

--- actors/enemy.gd
+++ actors/enemy.gd
@@ -1,40 +1,44 @@
 extends CharacterBody2D
+class_name EnemyBase
 
-const SPEED = 900.0
+const SPEED = 700.0
 const JUMP_VELOCITY = -400.0
 
-@onready var wall_detector := $wall_detector as RayCast2D
-@onready var texture := $texture as Sprite2D
-@onready var anim := $anim as AnimationPlayer
+@onready var anim := $anim
 
 @export var enemy_score := 100
 
+var wall_detector
+var texture
 var direction := -1
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
+func _apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func movement(delta):
+	velocity.x = direction * SPEED * delta
+	move_and_slide()
+
+func flip_direction():
 	if wall_detector.is_colliding():
 		direction *= -1
-		wall_detector.scale.x *= -1	
+		wall_detector.scale.x *= -1
 	
 	if direction == 1:
 		texture.flip_h = true
 	else:
 		texture.flip_h = false
-		
-	velocity.x = direction * SPEED * delta
 
-	move_and_slide()
-
-
-func _on_anim_animation_finished(anim_name: StringName):
+func kill_ground_enemy(anim_name: StringName):
 	if anim_name == "hurt":
-		Globals.score += enemy_score
-		queue_free()
+		kill_and_score()
+
+func kill_air_enemy():
+	kill_and_score()
+
+func kill_and_score():
+	Globals.score += enemy_score
+	queue_free()
--- scripts/rocket_cherry.gd
+++ scripts/rocket_cherry.gd
@@ -1,13 +1,4 @@
-extends CharacterBody2D
+extends EnemyBase
 
-@onready var anim = $anim as AnimatedSprite2D
-@export var enemy_score := 100
-
-func _on_hitbox_body_entered(body):
-	anim.play("hurt")
-
-
-func _on_anim_animation_finished():
-	if anim.animation == "hurt":
-		queue_free()
-		Globals.score += enemy_score
+func _ready():
+	anim.animation_finished.connect(kill_air_enemy)
