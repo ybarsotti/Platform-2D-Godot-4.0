93048335cdffab95b3c56fbb3f003e22273f3b56
Yuri Barsotti <yuribarsotti@gmail.com>
2024-04-24 22:42:08 -0300

    add title screen

---
+++ scripts/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_btn_pressed():
+	get_tree().change_scene_to_file("res://levels/world_01.tscn")
+
+
+func _on_credits_btn_pressed():
+	pass # Replace with function body.e
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
