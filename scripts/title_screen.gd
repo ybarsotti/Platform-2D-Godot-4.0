c8a93c3a19d267cc0844e797eed56ce0b9839183
Yuri Barsotti <yuribarsotti@gmail.com>
2024-05-19 21:52:00 -0300

    add sound to title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,21 +1,26 @@
 extends Control
 
+@onready var audio_stream_player = $AudioStreamPlayer as AudioStreamPlayer
+
 func _ready():
+	audio_stream_player.play()
 	Globals.coins = 0
 	Globals.score = 0
 	Globals.player_life = 3
 
 func _on_start_btn_pressed():
+	audio_stream_player.stop()
 	get_tree().change_scene_to_file("res://levels/world_01.tscn")
 
 
 func _on_credits_btn_pressed():
+	audio_stream_player.stop()
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
+func _on_settings_btn_pressed():
+	audio_stream_player.stop()
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _on_quit_btn_pressed():
+	audio_stream_player.stop()
 	get_tree().quit()
-
-
-func _on_settings_btn_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
